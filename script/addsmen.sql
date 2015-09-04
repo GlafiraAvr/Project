4776e7cb79ea72c99d46af595951f713f29fdcfc
GlafiraAvr <glafiraafraid@gmail.com>
2015-09-04 16:55:48 +0300

    Add script

---
+++ script/addsmen.sql
@@ -0,0 +1,27 @@
+SET SQL DIALECT 3;
+create domain TypeSShift
+as char(2)
+check ((value = 'V') or (value = 'K')   );
+
+
+drop table ServantTable;
+
+create  table ServantTable (
+   id smallint not null,
+   shiftnumber smallint not null,
+   shiftdate timestamp not null,
+   shiftType TypeSShift not null,
+   constraint pk_people primary key (id)
+
+);
+
+insert into  ServantTable
+(id,shiftnumber, shiftdate,shiftType)
+values
+(1,1,current_timestamp,'V');
+
+insert into  ServantTable
+(id,shiftnumber, shiftdate,shiftType)
+values
+(2,1,current_timestamp,'K');
+
